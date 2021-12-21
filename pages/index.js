211d20d5f478ff20790b2f7942c1f80ff8608395
shoaibbadshah <shoaibbadshah45@gmail.com>
2021-12-21 22:36:52 +0500

    chabot & responsive design

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import emailjs from "emailjs-com";
 import style from "../styles/Home.module.css";
 import logo from "../public/logo.png";
 import portfolio from "../public/portfolio.png";
+import { Drift } from "react-drift";
+
+// import Drift from "react-driftjs";
 
 export default function Home() {
   const sendEmail = (e) => {
@@ -29,22 +32,23 @@ export default function Home() {
 
   return (
     <div>
+      {/* <Drift appId="anwzpfzknn4p" /> */}
       <div className={style.main__img}>
-        <div className="ml-64">
+        <div className="text-center md:text-left md:ml-10 lg:ml-20 xl:ml-64">
           <Image src={logo} width={220} objectFit="contain" height={97} />
         </div>
 
-        <div className="flex">
+        <div className="md:flex">
           {/* left side */}
-          <div className="ml-64 w-1/2">
-            <h1 className="text-6xl font-bold text-white mt-10 ">
-              Download our app <br /> development portfolio.
+          <div className="md:ml-10 lg:ml-20 xl:ml-64 lg:w-1/2">
+            <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-white mt-10 text-center md:text-left hidden md:flex">
+              Download our app development portfolio.
             </h1>
-            <p className="text-white mt-10 text-2xl">
+            <p className="text-white mt-5 md:mt-7 lg:mt-10 text-sm md:text-lg lg:2xl text-center md:text-left hidden md:flex">
               Complete the following form to download our app and software
               development portfolio.
             </p>
-            <div className="text-center mt-10">
+            <div className="text-center mt-10 xl:mb-40">
               <Image
                 className="text-center"
                 src={portfolio}
@@ -56,12 +60,12 @@ export default function Home() {
           </div>
 
           {/* right side */}
-          <div className="w-1/2">
-            <div className="bg-gray-50 w-fit p-6 border rounded-md">
+          <div className="flex justify-center p-5 md:mr-3 lg:w-1/2">
+            <div className="bg-gray-50 w-fit h-min p-3 md:p-6 border rounded-md">
               <h2 className="text-black text-xl font-bold">
                 Download Our Portfolio
               </h2>
-              <p className="text-black text-lg mt-3">
+              <p className="text-black lg:text-lg mt-3">
                 To access our portfolio of mobile apps
                 <br /> and web projects, let us know about
                 <br /> you and your business.
@@ -70,15 +74,17 @@ export default function Home() {
               <form onSubmit={sendEmail} className="mt-8">
                 <input
                   type="text"
-                  className="border p-3 text-xl w-40 rounded"
+                  className="border pl-3 p-2 md:p-3 md:text-xl w-full lg:w-40 rounded"
                   name="first_name"
                   required
                   placeholder="First Name *"
                 />
 
+                <br className="md:hidden" />
+
                 <input
                   type="text"
-                  className="border p-3 text-xl ml-5 w-40 rounded"
+                  className="border pl-3 p-2 md:p-3 md:text-xl mt-5 lg:ml-5 w-full lg:w-40 rounded"
                   name="last_name"
                   required
                   placeholder="Last Name *"
@@ -87,7 +93,7 @@ export default function Home() {
 
                 <input
                   type="email"
-                  className="border p-3 text-xl mt-5 w-full rounded"
+                  className="border pl-3 p-2 md:p-3 md:text-xl mt-5 w-full rounded"
                   name="user_email"
                   required
                   placeholder="Work Email *"
@@ -96,7 +102,7 @@ export default function Home() {
 
                 <input
                   type="tel"
-                  className="border p-3 text-xl mt-5 w-full rounded"
+                  className="border pl-3 p-2 md:p-3 md:text-xl mt-5 w-full rounded"
                   name="phone"
                   required
                   placeholder="Phone *"
@@ -105,7 +111,7 @@ export default function Home() {
 
                 <input
                   type="text"
-                  className="border p-3 text-xl mt-5 w-full rounded"
+                  className="border pl-3 p-2 md:p-3 md:text-xl mt-5 w-full rounded"
                   name="company_name"
                   required
                   placeholder="Company Name *"
@@ -114,22 +120,30 @@ export default function Home() {
                 <input
                   type="submit"
                   value="Download Now"
-                  className="border p-3 text-xl mt-5 text-white font-bold bg-red-500 rounded px-12"
+                  className="border pl-3 p-2 md:p-3 md:text-xl mt-5 text-white font-bold bg-red-500 rounded px-5 md:px-12"
                 />
               </form>
             </div>
           </div>
         </div>
       </div>
-      <p className="ml-64">
+      <p className="text-center md:text-left md:ml-10 lg:ml-20 xl:ml-64 mt-2 mb-3 flex flex-col md:flex-row">
         Copyright © 2021 Alpha Bravo Development, LLC |
-        <a href="" className="text-blue-700 m-5">
+        <a href="" className="text-blue-700 ml-5 mr-5">
           Privacy Policy
         </a>
         <a href="#" className="text-blue-700 ">
           Contact Us
         </a>
       </p>
+      <div className="w-min">
+        <Drift
+          appId="anwzpfzknn4p"
+          userId="123"
+          attributes={{ email: "paul@borza.ro" }}
+          config={{ locale: "en-US" }}
+        />
+      </div>
     </div>
   );
 }
